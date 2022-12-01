fc06c5e6289a8b3f0969069c9db921e3a39f9fca
Rainer Koenig <rainer.konig@suse.com>
2022-12-01 11:10:51 +0100

    Fix for problems in iscsi_client
    - missing "use YaST::workarounds" fixed

--- tests/iscsi/iscsi_client.pm
+++ tests/iscsi/iscsi_client.pm
@@ -29,6 +29,7 @@ use scheduler 'get_test_suite_data';
 use y2_mm_common 'prepare_xterm_and_setup_static_network';
 use YaST::workarounds;
 use Utils::Logging 'save_and_upload_log';
+use YaST::workarounds;
 
 # load expected test data from yaml
 # common for both iscsi MM modules
