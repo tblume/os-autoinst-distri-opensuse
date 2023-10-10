164ed6d907ae4e5b01405aa12240ab5659e85ba4
root <Thomas.Blume@suse.com>
2023-10-10 09:48:18 +0200

    add new tests for v254

---
+++ tests/systemd_testsuite/v254/test_80_notifyaccess.pm
@@ -0,0 +1,34 @@
+# SUSE's openQA tests
+#
+# Copyright 2020 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Run test executed by TEST-80-NOTIFYACCESS from upstream after openSUSE/SUSE patches.
+# Maintainer: Sergio Lindo Mansilla <slindomansilla@suse.com>, Thomas Blume <tblume@suse.com>
+
+use base 'systemd_testsuite_test';
+use warnings;
+use strict;
+use testapi;
+
+sub pre_run_hook {
+    my ($self) = @_;
+    #prepare test
+    $self->testsuiteprepare('TEST-80-NOTIFYACCESS');
+}
+
+sub run {
+    #run test
+    my $timeout = get_var('SYSTEMD_TEST_DEFAULT_TIMEOUT') || 120;
+    assert_script_run 'cd /usr/lib/systemd/tests/integration-tests';
+    assert_script_run 'export NO_BUILD=1 && make -C TEST-80-NOTIFYACCESS run 2>&1 | tee /tmp/testsuite.log', $timeout;
+    assert_script_run 'grep "TEST-80-NOTIFYACCESS RUN: .* \[OK\]" /tmp/testsuite.log';
+    script_run 'export NO_BUILD=1 && make -C TEST-80-NOTIFYACCESS clean';
+}
+
+sub test_flags {
+    return {always_rollback => 1};
+}
+
+
+1;
---
+++ tests/systemd_testsuite/v254/test_82_softreboot.pm
@@ -0,0 +1,34 @@
+# SUSE's openQA tests
+#
+# Copyright 2020 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Summary: Run test executed by TEST-82-SOFTREBOOT from upstream after openSUSE/SUSE patches.
+# Maintainer: Sergio Lindo Mansilla <slindomansilla@suse.com>, Thomas Blume <tblume@suse.com>
+
+use base 'systemd_testsuite_test';
+use warnings;
+use strict;
+use testapi;
+
+sub pre_run_hook {
+    my ($self) = @_;
+    #prepare test
+    $self->testsuiteprepare('TEST-82-SOFTREBOOT');
+}
+
+sub run {
+    #run test
+    my $timeout = get_var('SYSTEMD_TEST_DEFAULT_TIMEOUT') || 120;
+    assert_script_run 'cd /usr/lib/systemd/tests/integration-tests';
+    assert_script_run 'export NO_BUILD=1 && make -C TEST-82-SOFTREBOOT run 2>&1 | tee /tmp/testsuite.log', $timeout;
+    assert_script_run 'grep "TEST-82-SOFTREBOOT RUN: .* \[OK\]" /tmp/testsuite.log';
+    script_run 'export NO_BUILD=1 && make -C TEST-82-SOFTREBOOT clean';
+}
+
+sub test_flags {
+    return {always_rollback => 1};
+}
+
+
+1;
